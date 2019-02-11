cb5ba0b92db93a9c0dbafb30f1fd41247fcda89c
Ariel Conti <arielconti10@gmail.com>
2019-02-11 08:16:59 -0200

    Timeline component base structure

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
 
+import twitterLogo from '../twitter.svg';
+import './Timeline.css';
+
 export default class Timeline extends Component {
+  state={
+    newTweet: ''
+  }
+
+  handleInputChange = e => {
+
+  }
+
+  handleNewTweet = e => {
+
+  }
+  
+
   render() {
     return (
-      <div>
-        <h1>Timeline</h1>
+      <div class="timeline-wrapper">
+        <img src={twitterLogo} height="24" alt="GoTwitter"/>
+
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}  
+            placeholder="O que está acontecendo?"
+          >
+
+          </textarea>
+        </form>
+
+        <ul>
+          
+        </ul>
       </div>
     )
   }
